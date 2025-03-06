71b1100628ac376137b69dcb634d338579e4a593
HWAI <kll5893@gmail.com>
2025-03-06 14:59:20 +0900

    아이콘 파일 추가

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        # # ✅ 파일 존재 여부 확인 (파일이 없으면 경고창 출력 후 종료)
-        # if not os.path.exists(self.current_file):
-        #     QMessageBox.critical(self, "파일 없음", f"데이터 파일 '{self.current_file}'이(가) 없습니다.\n파일을 확인하세요.")
-        #     sys.exit(1)  # 프로그램 종료
